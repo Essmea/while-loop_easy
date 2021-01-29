ee7f6ed210984be2f9243b059237d49d3a3e4022
Essmea <77241696+Essmea@users.noreply.github.com>
2021-01-29 11:43:58 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Meny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ett_till_tre = true;
+            while (ett_till_tre)
+            {
+                Console.WriteLine("Välj 1 för att välja ett");
+                Console.WriteLine("Välj 2 för att välja två");
+                Console.WriteLine("Välj 3 för att välja tre och avsluta programmet");
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 1)
+                    Console.WriteLine("Du valde ett!");
+
+                else if (input == 2)
+                    Console.WriteLine("Du valde två! Bra jobbat!");
+
+                else
+                    ett_till_tre = false; //falskt = stänger av programmet 
+            
+            }
+
+        }
+    }
+}
